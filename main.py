d3c6d7392d04ca53eb5bf24f3a92ed257b91ce2f
0dayig0r <igormedeiros.f@gmail.com>
2026-02-27 21:14:13 -0300

    IA Image analysis

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ from providers import openai
 
 
 # print(anthropic.test())
-book = openai.book_finder('O livro menciona 42 como o sentido da vida')
-if book:
-    print(f"Titulo: {book.title}, Autor: {book.author}")
 
-else:
-    print("Nenhum livro encontrado.")
+# book = openai.book_finder('O livro menciona 42 como o sentido da vida')
+# if book:
+#     print(f"Titulo: {book.title}, Autor: {book.author}")
 
+# else:
+#     print("Nenhum livro encontrado.")
+
+print(openai.image_reader('https://recreio.com.br/wp-content/uploads/2024/05/goku_capa-1.jpg'))
--- providers/openai.py
+++ providers/openai.py
@@ -5,6 +5,19 @@ from pydantic import BaseModel
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+
+
+def image_reader(url):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "system", "content": "Você é um assistente de análise de imagens. Dada uma imagem, identifique os objetos presentes e extraia qualquer texto visível. Não forneça nenhuma informação adicional, apenas a descrição dos objetos e o texto extraído."},
+                  {"role": "user", "content": [{"type": "image_url", "image_url": {"url": url}}, {"type": "text", "text": f"Analise esta imagem e identifique os objetos presentes e extraia qualquer texto visível."}]}]
+    )
+    return response.choices[0].message.content
+
+
+
+
 class BookResponse(BaseModel):
     title: str
     author: str
@@ -13,7 +26,7 @@ def book_finder(search):
     res = client.chat.completions.parse(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "Você é um assistente de busca de livros. Dado um termo de pesquisa, retorne o título e o autor do livro mais relevante."},
+            {"role": "system", "content": "Você é um assistente de busca de livros. Dado um termo de pesquisa, retorne o título e o autor do livro mais relevante. Não retorne nenhuma mensagem adicional, apenas o título e o autor.   "},
             {"role": "user", "content": f"Encontre um livro sobre '{search}'."}
         ],
         response_format=BookResponse
