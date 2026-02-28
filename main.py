49d81fbd9b8aca6d313b46af3c7db1c05491722d
0dayig0r <igormedeiros.f@gmail.com>
2026-02-27 21:05:06 -0300

    using BaseModel in the structure

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ load_dotenv()
 
 
 # from providers import anthropic
-# from providers import openai
-from providers import xai  
+from providers import openai
+# from providers import xai  
 # from providers import google
 
 
 # print(anthropic.test())
-print(xai.test())
+book = openai.book_finder('O livro menciona 42 como o sentido da vida')
+if book:
+    print(f"Titulo: {book.title}, Autor: {book.author}")
+
+else:
+    print("Nenhum livro encontrado.")
 
--- providers/openai.py
+++ providers/openai.py
@@ -1,19 +1,42 @@
 import os
-from dotenv import load_dotenv
 from openai import OpenAI
 
-load_dotenv()
+from pydantic import BaseModel
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def test():
-    response = client.responses.create(
-        model="gpt-4o-mini",
-        input=[
-            {"role": "system", "content": "Seja direto e conciso. Responda apenas o necessário."},
-            {"role": "user", "content": "Qual é a capital da França?"}
-        ],
-    )
-    return response.output_text 
+class BookResponse(BaseModel):
+    title: str
+    author: str
 
-print(test())
+def book_finder(search):
+    res = client.chat.completions.parse(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "Você é um assistente de busca de livros. Dado um termo de pesquisa, retorne o título e o autor do livro mais relevante."},
+            {"role": "user", "content": f"Encontre um livro sobre '{search}'."}
+        ],
+        response_format=BookResponse
+    )
+    
+    message = res.choices[0].message
+    
+    if message.parsed:
+        return message.parsed
+    else:
+        return False
+  
+    
+
+
+# def test():
+#     response = client.responses.create(
+#         model="gpt-4o-mini",
+#         input=[
+#             {"role": "system", "content": "Seja direto e conciso. Responda apenas o necessário."},
+#             {"role": "user", "content": "Qual é a capital da França?"}
+#         ],
+#     )
+#     return response.output_text 
+
+# print(test())
